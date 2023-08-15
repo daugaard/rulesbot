8681e715d7de933f8e59b876750ea2885cd28e67
Søren Blond Daugaard <soren@practicalai.io>
2023-08-14 20:59:51 -0400

    Add an extra test.

--- games/tests.py
+++ games/tests.py
@@ -1,9 +1,12 @@
 import shutil
 from unittest import mock
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
 from django.urls import reverse
+from django.contrib.admin.sites import AdminSite
+from django.contrib.messages.storage.fallback import FallbackStorage
 
 from langchain.document_loaders import PyPDFLoader
+from games.admin import GameAdmin
 
 from games.models import Document, Game
 from games.services.document_ingestion_service import ingest_document
@@ -123,3 +126,69 @@ class GameVectorStoreTest(TestCase):
         self.assertEqual(result[0].metadata["page"], 0)
         self.assertEqual(result[0].metadata["game_id"], game.id)
         self.assertEqual(result[0].metadata["document_id"], 0)
+
+
+class GameAdminTest(TestCase):
+    def get_ingest_documents_request(self):
+        request = RequestFactory().get("/admin/games/game")
+        setattr(request, "session", "session")
+        messages = FallbackStorage(request)
+        setattr(request, "_messages", messages)
+        return request
+
+    def test_ingest_documents(self):
+        game = Game.objects.create(name="Test Game")
+        document = Document.objects.create(game=game, url="some-url")
+
+        # Patch / mock document ingestion._download_to_file and replace it with a function that returns a file with the contents of the test.pdf file
+        with mock.patch(
+            "games.services.document_ingestion_service._download_to_file"
+        ) as _download_to_file_mock:
+            _download_to_file_mock.side_effect = lambda _, file: shutil.copyfile(
+                "games/fixtures/test.pdf", file.name
+            )
+            GameAdmin(Game, AdminSite()).ingest_documents(
+                self.get_ingest_documents_request(), [game]
+            )
+
+        game.refresh_from_db()
+
+        # try to make a similarity search for the document
+        results = game.vector_store.index.similarity_search("page 1")
+
+        self.assertEqual(len(results), 2)
+        self.assertEqual(results[0].metadata["page"], 0)
+        self.assertEqual(results[0].metadata["game_id"], game.id)
+        self.assertEqual(results[0].metadata["document_id"], document.id)
+        self.assertTrue("Page\n1" in results[0].page_content)
+
+    def test_ingest_documents_twice(self):
+        game = Game.objects.create(name="Test Game")
+        document = Document.objects.create(game=game, url="some-url")
+
+        # Patch / mock document ingestion._download_to_file and replace it with a function that returns a file with the contents of the test.pdf file
+        with mock.patch(
+            "games.services.document_ingestion_service._download_to_file"
+        ) as _download_to_file_mock:
+            _download_to_file_mock.side_effect = lambda _, file: shutil.copyfile(
+                "games/fixtures/test.pdf", file.name
+            )
+            GameAdmin(Game, AdminSite()).ingest_documents(
+                self.get_ingest_documents_request(), [game]
+            )
+
+            # Call it a second time
+            GameAdmin(Game, AdminSite()).ingest_documents(
+                self.get_ingest_documents_request(), [game]
+            )
+
+        game.refresh_from_db()
+
+        # try to make a similarity search for the document
+        results = game.vector_store.index.similarity_search("page 1")
+
+        self.assertEqual(len(results), 2)  # Still should only show 2 results
+        self.assertEqual(results[0].metadata["page"], 0)
+        self.assertEqual(results[0].metadata["game_id"], game.id)
+        self.assertEqual(results[0].metadata["document_id"], document.id)
+        self.assertTrue("Page\n1" in results[0].page_content)
