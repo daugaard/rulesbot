c77825387e920ad6f9a566c6a8e7de2666ada8fa
Søren Blond Daugaard <soren@practicalai.io>
2023-08-16 13:18:37 -0400

    Add display url to documents.

--- games/models.py
+++ games/models.py
@@ -21,7 +21,8 @@ class Game(models.Model):
 class Document(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     display_name = models.CharField(max_length=500)
-    url = models.CharField(max_length=500)
+    url = models.CharField(max_length=500)  # URL to ingest from
+    public_url = models.CharField(max_length=1000, null=True)  # URL to display to users
     ingested = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
@@ -29,3 +30,7 @@ class Document(models.Model):
 
     def __str__(self):
         return f"{self.display_name}"
+
+    @property
+    def display_url(self):
+        return self.public_url or self.url
