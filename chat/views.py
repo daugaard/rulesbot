d28163d92c51691267d843a39ed87c2e34ba5462
Søren Blond Daugaard <soren@practicalai.io>
2023-08-18 13:58:54 -0400

    Stylesheet theme files and initial master style.

--- chat/views.py
+++ chat/views.py
@@ -24,6 +24,14 @@ class SessionIndexView(generic.ListView):
         return ChatSession.objects.order_by("-created_at")
 
 
+class LandingView(generic.ListView):
+    template_name = "chat/landing.html"
+    context_object_name = "games"
+
+    def get_queryset(self):
+        return Game.objects.order_by("-created_at")[:8]
+
+
 def view_chat_session(request, session_slug):
     chat_session = get_object_or_404(ChatSession, session_slug=session_slug)
 
