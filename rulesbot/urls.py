97c96953b14401b14f31cb8625abccfda11a13b1
Søren Blond Daugaard <soren@practicalai.io>
2023-08-29 20:40:18 -0400

    Add robots file.

--- rulesbot/urls.py
+++ rulesbot/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 from games.sitemaps import GamesSitemap
 from pages import views as pages_views
@@ -34,5 +35,10 @@ urlpatterns = [
         sitemap,
         {"sitemaps": {"pages": PagesSitemap, "games": GamesSitemap}},
         name="django.contrib.sitemaps.views.sitemap",
+    ),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+        name="robots.txt",
     ),
 ]
