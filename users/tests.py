61688936ef82343c951478383d63840286bd6d23
Søren Blond Daugaard <soren@practicalai.io>
2023-10-02 16:06:13 -0400

    More user tests.

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
 
@@ -7,6 +8,58 @@ class TestViews(TestCase):
         response = self.client.get("/users/sign-up")
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "users/sign_up.html")
+
+    def test_post_signup_page(self):
+        self.assertEqual(User.objects.count(), 0)
+        response = self.client.post(
+            "/users/sign-up",
+            {
+                "username": "testuser",
+                "email": "test@email.com",
+                "password": "testpassword",
+                "confirm_password": "testpassword",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, "/users/account")
+
+        user = User.objects.get(username="testuser")
+        self.assertEqual(user.username, "testuser")
+        self.assertEqual(user.email, "test@email.com")
+
+    def test_post_signup_page_with_existing_username(self):
+        User.objects.create_user(
+            username="testuser", email="test@email.com", password="testpassword"
+        )
+        response = self.client.post(
+            "/users/sign-up",
+            {
+                "username": "testuser",
+                "email": "test@email2.com",
+                "password": "testpassword",
+                "confirm_password": "testpassword",
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "users/sign_up.html")
+        self.assertFormError(response, "form", "username", "Username already exists")
+
+    def test_post_signup_page_with_existing_email(self):
+        User.objects.create_user(
+            username="testuser", email="test@email.com", password="testpassword"
+        )
+        response = self.client.post(
+            "/users/sign-up",
+            {
+                "username": "testuser2",
+                "email": "test@email.com",
+                "password": "testpassword",
+                "confirm_password": "testpassword",
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "users/sign_up.html")
+        self.assertFormError(response, "form", "email", "Email already exists")
 
     def test_get_login_page(self):
         response = self.client.get("/users/login")
